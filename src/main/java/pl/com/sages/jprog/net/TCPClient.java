3ba342a5f2f1807a4f82be06d638a2e49dc2e016
dzierzy <dzierzy@o2.pl>
2016-10-06 14:56:37 +0200

    interactive tcp

--- src/main/java/pl/com/sages/jprog/net/TCPClient.java
+++ src/main/java/pl/com/sages/jprog/net/TCPClient.java
@@ -3,8 +3,10 @@ package pl.com.sages.jprog.net;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.util.Scanner;
 
 /**
  * Created by marcin on 05.10.2016.
@@ -13,13 +15,18 @@ public class TCPClient {
 
     public static void main(String[] args) {
         try {
-            Socket s = new Socket("127.0.0.1", 3000);
+            Socket s = new Socket("192.168.1.114", 3000);
             BufferedReader reader = new BufferedReader(new InputStreamReader(s.getInputStream()));
+            PrintWriter writer = new PrintWriter(s.getOutputStream());
+            Scanner scanner = new Scanner(System.in);
+
             String line = null;
             // Wywołanie blokujące (readLine)
             while ((line = reader.readLine()) != null) {
                 System.out.println(line);
+                writer.write(scanner.nextLine());
             }
+            // 192.168.1.212
 
         } catch (UnknownHostException e) {
         } catch (IOException e) {
--- src/main/java/pl/com/sages/jprog/net/TCPServer.java
+++ src/main/java/pl/com/sages/jprog/net/TCPServer.java
@@ -1,9 +1,12 @@
 package pl.com.sages.jprog.net;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Scanner;
 
 /**
  * Created by marcin on 05.10.2016.
@@ -16,8 +19,27 @@ public class TCPServer {
             while(true) {
                 // Wywołanie blokujące (accept)
                 Socket connection = server.accept();
+                System.out.println("connected...");
+
                 PrintWriter writer = new PrintWriter(	connection.getOutputStream());
+                BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+
                 writer.println("Hello!");
+                writer.flush();
+
+                Scanner scanner = new Scanner(System.in);
+                String msg = "";
+                String received = "";
+                while(!"quit".equalsIgnoreCase(msg) && received!=null){
+                    System.out.print(">> ");
+                    msg=scanner.nextLine();
+                    writer.println(msg);
+                    writer.flush();
+                    received = reader.readLine();
+                    System.out.println("received: " + received);
+
+                }
+
                 writer.flush();
                 connection.close();
             }
